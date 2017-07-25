b48820cc9a0f50a6f0ae9b05829937e6e827f732
Admir Memic <admir.memic@springer.com>
2017-07-25 14:47:25 +0200

    Logging

--- src/main/scala/com/admir/is24crawler/Crawler.scala
+++ src/main/scala/com/admir/is24crawler/Crawler.scala
@@ -2,6 +2,7 @@ package com.admir.is24crawler
 
 import java.text.{DecimalFormat, DecimalFormatSymbols}
 
+import akka.event.slf4j.SLF4JLogging
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Sink, Source}
 import com.admir.is24crawler.models.Expose
@@ -12,7 +13,7 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
-class Crawler(implicit materializer: Materializer, ec: ExecutionContext) {
+class Crawler(implicit materializer: Materializer, ec: ExecutionContext) extends SLF4JLogging {
   val df = new DecimalFormat()
   val sfs = new DecimalFormatSymbols
   sfs.setGroupingSeparator('.')
@@ -28,10 +29,23 @@ class Crawler(implicit materializer: Materializer, ec: ExecutionContext) {
 
     Source.single(browser.get(host + firstPagePath))
       .map(doc => Try((doc >> element("#pageSelection > select") >> elementList("option")) >> attr("value")).toOption)
-      .collect { case Some(values) => values }.mapConcat(identity)
-      .map(pagePath => browser.get(host + pagePath))
+      .collect {
+        case Some(values) =>
+          log.info(s"Result has following pages:\n${values.reduceLeft((acc, link) => s"$acc\n$link")}")
+          values
+      }
+      .mapConcat(identity)
+      .map(pagePath => {
+        val url = host + pagePath
+        log.info(s"Requesting result page: $url")
+        browser.get(url)
+      })
       .mapConcat(_ >> elementList("article.result-list-entry") >> attr("data-obid"))
-      .map(objId => (browser.get(s"$host/expose/$objId"), objId))
+      .map(objId => {
+        val url = s"$host/expose/$objId"
+        log.info(s"Requesting detail page: $url")
+        (browser.get(url), objId)
+      })
       .map { case (doc, objId) => (doc >> text("dd.is24qa-gesamtmiete"), s"$host/expose/$objId") }
       .map {
         case (priceStr, link) =>
